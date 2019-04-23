41d0489810d7224dfc18fe8000e6dd14f39befcf
liuderek97@gmail.com <liuderek97@gmail.com>
2019-04-23 15:41:22 +1000

    Updated several User class and account class methods add print functionality

--- bank_app.rb
+++ bank_app.rb
@@ -12,17 +12,17 @@ def start_app
     puts "Please enter your password"
     
     elsif user_response.include?("create")
-  	    User.create_account
-	end
+  	  User.create_account
+	  end
 
 		puts "What would you like to do? (options: Balance, Deposit, Withdraw)"
 		user_response = gets.chomp.downcase
 
 		if user_response == "balance"
-    	    puts balance
+    	puts balance
 		elsif
-    	    puts "Invalid selection"
-        end
+    	puts "Invalid selection"
+    end
 end
 
 class User
@@ -39,48 +39,64 @@ class User
     @age
  end
 
- def self.create_account
+  def self.create_account
+
     puts "Please enter your First Name"
-    user_first_name = gets.chomp
+    @@user_first_name = gets.chomp
     puts"Please enter your Last Name"
-    user_last_name = gets.chomp
-    puts "Welcome #{user_first_name} Please enter your age"
-    user_age = gets.chomp
+    @@user_last_name = gets.chomp
+
+    puts "Welcome #{@@user_first_name} Please enter your age"
+    @@user_age = gets.chomp
     system "clear"
     puts "Please enter a username"
     user_name = gets.chomp
     puts"Please enter the password you would like to use"
     user_password = gets.chomp
     @@balance = 0.0
+    
+    #create an Account Object using the username userpassword and balance
     user_first_name = Account.new(user_name, user_password, @@balance)
-end
+    puts user_first_name.user_name
+    puts user_first_name.password
+    puts user_first_name.balance
+    puts "would you like to print you user details"
+    user_response = gets.chomp
+    if user_response == "y"
+      print_user_details
+    end
+  end
   
+  def self.print_user_details
+    puts "First Name: #{@@user_first_name}"
+    puts "Last Name: #{@@user_last_name}"
+    puts "Age: #{@@user_age}"
+  end
 end
 
 class Account
 
     def initialize(user_name, password, balance)
-        @user_name = user_name
-        @password = password
-        @balance = balance
+      @user_name = user_name
+      @password = password
+      @balance = balance
     end
 
     def user_name
-        @user_name
+      @user_name
     end
 
     def password
-        @password
+      @password
     end
     
     def balance
-        @balance
+      @balance
     end
 
     def new_account_number
-        unique_id = 6.times.map { rand(0..7) }.join.to_i
+      unique_id = 6.times.map { rand(0..7) }.join.to_i
     end
-
 end
 
 start_app
