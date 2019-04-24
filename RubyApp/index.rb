f0214a0d65453fa76a7e4de03a1428cafc892d5c
AidanL <aidanlawson31@gmail.com>
2019-04-24 16:27:30 +1000

    commit1

--- RubyApp/index.rb
+++ RubyApp/index.rb
@@ -1,4 +1,5 @@
-require_relative './model.rb'
+# require '/Users/aidans_mac/gentech_2019/Ruby-Application-Assignment-1/RubyApp/models.rb'
+require_relative 'models.rb'
 
 include Account
-Account.create_account(:hello, 40)
+Account::create_account(:hello, 40)
